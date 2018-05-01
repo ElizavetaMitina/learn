0b8c4d5da6998197b554d982f5d7b3d8ca7f68b3
Elizaveta Mitina <tim445406@gmail.com>
2018-05-01 14:25:57 +0300

    Task11js

--- Lesson11/JS/Task.js
+++ Lesson11/JS/Task.js
@@ -34,22 +34,15 @@ for (var y = 2014; y <= 2050; y++){
                 break;
             }
 }
-var ny = new Date(2019, 0, 1);
 //4
+var ny = new Date(2019, 0, 1);
 console.log((Math.round((ny-now)/86400000)) + ' days before the New Year');
 //5
-function  is_array(text){
-var arr3 = text;
-    if (arr3.length > 1){
-        return true;
-    }
-    else {
-        return false;
-    }
+function is_array(mass) {
+    return Array.isArray(mass);
 }
-var qwe = {0:'q', 1:'w', 2:'e'};
-    console.log(is_array(qwe));
-    console.log(is_array([1, 2, 4, 0]));
+console.log(is_array('qwe'));
+console.log(is_array([1, 2, 4, 0]));
 //6
 numbers = [5, 4, 3, 2, 1];
 function clone(mass) {
@@ -139,3 +132,143 @@ function compareFunction(a, b) {
 }
 library.sort(compareFunction);
 console.log(library);
+//13
+arr7 = [1, 1, 2, 3];
+arr8 = [4, 5, 6, 6, 7];
+Newarr = arr7.concat(arr8);
+console.log(duplicate(Newarr));
+//14
+arr9 = ['yes', 'I', 'do not', 'love', 'you'];
+function remove(mass, str) {
+    var number = mass.indexOf(str);
+    mass.splice(number, 1);
+    return mass;
+}
+console.log(remove(arr9, 'do not'));
+//15
+function random(mass) {
+    var m = Math.floor(Math.random()*mass.length);
+    return mass[m];
+}
+console.log(random(Newarr));
+//16
+function move(mass, number) {
+    var m = Math.floor(Math.random()*mass.length);
+    var num = mass[number];
+    mass[number] = mass[m];
+    mass[m] = num;
+    return mass;
+}
+console.log(move(Newarr, 2));
+//17
+var date2 = new Date(2018, 7, 21);
+function diff(date1, date2) {
+    return (Math.round((date2-date1)/86400000));
+}
+console.log(diff(now, date2));
+//18
+function maxDate(mass) {
+    var max = 0;
+    for (m = 0; m < mass.length; m++){
+        mass [m] = new Date(mass[m]);
+        if (mass[m] > mass[max]){
+            max = m;
+        }
+    }
+    return max;
+}
+console.log(['2015/02/01', '2015/02/02', '2015/01/03'][maxDate(['2015/02/01', '2015/02/02', '2015/01/03'])]);
+//19
+str2 = 'When chebureck is ready I am happy';
+function split(str) {
+    str = str.split(' ');
+    return str;
+}
+console.log(split(str2));
+//20
+str3 = 'sasha loves me';
+function capitalize(str) {
+    var mass = str.split('');
+    mass[0] = mass[0].toUpperCase();
+    str = mass.join('');
+    return str;
+}
+console.log(capitalize(str3));
+//21
+function camelize(str) {
+    var mass = str.split(' ');
+    for (i = 0; i < mass.length; i++){
+        if (mass[i] === '-'){
+            mass.splice(i, 1);
+        }
+    }
+    str = mass.join('');
+    return str;
+}
+console.log(camelize("Java Script"));
+console.log(camelize("java-script"));
+console.log(camelize("Java Script Exercises"));
+//22
+arr11 = [1, 3, 10, 13, 4, 2];
+function highest(mass) {
+    var max = 0;
+    for (i = 0; i < mass.length; i++){
+        if (mass[i] > mass[max]){
+            max = i;
+        }
+    }
+    return max;
+}
+console.log('Наибольший элемент массива (' + arr11 + ') - ' + arr11[highest(arr11)]);
+//23
+function lowest(mass) {
+    var min = 0;
+    for (i = 0; i < mass.length; i++){
+        if (mass[i] < mass[min]){
+            min = i;
+        }
+    }
+    return min;
+}
+console.log('Наименьший элемент массива (' + arr11 + ') - ' + arr11[lowest(arr11)]);
+//24
+first = 1;
+second = '2';
+third = 'three';
+function num(x) {
+    return (typeof x === 'number')
+}
+console.log(num(first));
+console.log(num(second));
+console.log(num(third));
+//25
+function sum(mass) {
+    var sum = 0;
+    for (i = 0; i < mass.length; i++){
+        sum = sum + mass[i];
+    }
+    return sum;
+}
+console.log('Сумма массива ('  + arr11 + ') - ' + sum(arr11));
+//26
+obj = {
+    name: 'Liza',
+    age: 19,
+    sex: 'very gooooood'
+};
+function length(object) {
+    var size = 0,
+    key;
+    for (key in object){
+        if (key in object) size++;
+    }
+    return size;
+}
+console.log(length(obj));
+//27
+function list(object) {
+    for (var key in object){
+        console.log(key + ': ' + object[key]);
+    }
+}
+list(obj);
